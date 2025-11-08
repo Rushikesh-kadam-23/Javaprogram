95aa5059d391433180cfb33a4da3e055181cd50d
Rushikesh Kadam <kadamrushikeshg6@gmail.com>
2025-11-08 12:49:58 +0530

    Initial commit~

---
+++ src/ReverseLeftHalfPyramidPattern.java
@@ -0,0 +1,17 @@
+public class ReverseLeftHalfPyramidPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int i,j;
+        for(i=n-1;i>=0;i--){
+            for(j=0;j<=n-1;j++){
+                System.out.print(" ");
+            }
+            for(j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
